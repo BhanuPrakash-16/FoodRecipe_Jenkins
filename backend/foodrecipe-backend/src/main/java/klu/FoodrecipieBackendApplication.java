380e0dafe0002fca95bc13b3c2bdd50ccc114051
Bhanu Prakash Alluri <bhanualluri2006@gmail.com>
2025-09-24 21:02:52 +0530

    Update FoodrecipieBackendApplication.java

--- backend/foodrecipe-backend/src/main/java/klu/FoodrecipieBackendApplication.java
+++ backend/foodrecipe-backend/src/main/java/klu/FoodrecipieBackendApplication.java
@@ -2,13 +2,19 @@ package klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FoodrecipieBackendApplication {
+public class FoodrecipieBackendApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FoodrecipieBackendApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodrecipieBackendApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(FoodrecipieBackendApplication.class, args);
 		System.out.println("Enjoy the Delicious Food.....");
-	}
-
+    }
 }
+
